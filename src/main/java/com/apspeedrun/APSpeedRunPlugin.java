68d78cc01818a59e733fd020795906f26c89d058
McMacMakes <ryanmcevoysecondary@gmail.com>
2025-06-10 23:59:18 -0400

    Added plugin info

--- src/main/java/com/apspeedrun/APSpeedRunPlugin.java
+++ src/main/java/com/apspeedrun/APSpeedRunPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.apspeedrun;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "AP Quest Speedrun Plugin"
 )
-public class ExamplePlugin extends Plugin
+public class APSpeedRunPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private APSpeedRunConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	APSpeedRunConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(APSpeedRunConfig.class);
 	}
 }
--- src/test/java/com/apspeedrun/APSpeedRunPluginTest.java
+++ src/test/java/com/apspeedrun/APSpeedRunPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.apspeedrun;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class APSpeedRunPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(APSpeedRunPlugin.class);
 		RuneLite.main(args);
 	}
 }
